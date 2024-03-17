9fee72617d77a11d2f1ea8a7fb63c2678d9a07e5
CarmenBasa <163778481+CarmenBasa@users.noreply.github.com>
2024-03-17 16:05:52 -0600

    Add files via upload

---
+++ Variable 2.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declarar la variable de nombre
+        String nombre = "Carmen";
+        
+        //Corregir el valor asignado a la variable nombre
+        nombre="Daniela";
+        
+        // Declarar la variable de edad
+        int edad = 18;
+        
+        // Imprimir el valor de las variables
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+    }
+}
+
