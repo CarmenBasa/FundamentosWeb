2aec83eca4f5d1f0ec11fbf707bae2d1562eab91
CarmenBasa <163778481+CarmenBasa@users.noreply.github.com>
2024-03-17 16:41:04 -0600

    Delete Variable 1.js

--- Variable 1.js
+++
@@ -1,8 +0,0 @@
-// Declarar una variable myName
-var myName;
-
-// Darle a myName un valor
-myName = "Carmen";
-
-// Declarar una variable llamada myAge y darle valor en la misma linea
-var myAge = 18;
