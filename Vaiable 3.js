96a7b153696e31b54470a8de77108c76b6836904
CarmenBasa <163778481+CarmenBasa@users.noreply.github.com>
2024-03-17 16:41:34 -0600

    Delete Vaiable 3.js

--- Vaiable 3.js
+++
@@ -1,9 +0,0 @@
-//Cambie const por var para aclarar que es una variable y no una constante
-var myName = 'Default';
-myName = 'Chris';
-
-//La segunda correccion fue quitar los '' del 42 por que al dejarlos se suma un texto con un numero, al quitarlos el codigo entiende que se suman 2 valores
-let myAge = 42;
-
-para1.textContent = myName;
-para2.textContent = `In 20 years, I will be ${myAge + 20}`;
